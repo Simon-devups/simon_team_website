2546d139af37fd0b53a406c9f70b8ae1e233a548
Aryan <aryanshafierad@gmail.com>
2026-06-23 19:52:59 +0330

    little resonsive

--- frontend/app/layout/Footer.jsx
+++ frontend/app/layout/Footer.jsx
@@ -1,26 +1,33 @@
 import styles from "./Footer.module.css"
 const Footer = () => {
     return (
-        <div>
-            <div>d</div>
+        <div className={styles.cont}>
+            <div className={styles.right}>
+                <div className={styles.logo}>
+                    <img src="/simon.svg" alt="logo" />
+                </div>
+                <p className={styles.introduce}>تیم سایمون کنار شماست تا با خلاقیت، استراتژی و تحلیل دقیق، برندتان را در دنیای دیجیتال ماندگار و تاثیرگذار کند.</p>
+            </div>
             <div className={styles.left}>
-                <div>
-                    <div>لینک های مر تبط</div>
-                    <div>پروژه موفق</div>
-                    <div>پروژه موفق</div>
-                    <div>پروژه موفق</div>
-                </div>
-                <div>
-                    <div>خدمات ما</div>
-                    <div>پروژه موفق</div>
-                    <div>پروژه موفق</div>
-                    <div>پروژه موفق</div>
-                </div>
-                <div>
-                    <div>ارتباط باما</div>
-                    <div>پروژه موفق</div>
-                    <div>پروژه موفق</div>
-                    <div>پروژه موفق</div>
+                <div className={styles.left__list}>
+                    <div className={styles.left__listCont}>
+                        <div>لینک های مر تبط</div>
+                        <span>پروژه موفق</span>
+                        <span>پروژه موفق</span>
+                        <span>پروژه موفق</span>
+                    </div>
+                    <div className={styles.left__listCont}>
+                        <div>خدمات ما</div>
+                        <span>پروژه موفق</span>
+                        <span>پروژه موفق</span>
+                        <span>پروژه موفق</span>
+                    </div>
+                    <div className={styles.left__listCont}>
+                        <div>ارتباط باما</div>
+                        <span>پروژه موفق</span>
+                        <span>پروژه موفق</span>
+                        <span>پروژه موفق</span>
+                    </div>
                 </div>
             </div>
         </div>
